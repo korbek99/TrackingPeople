4a4b5a52ed723fc7f2471957631f504aab259d9d
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-10-03 19:08:08 -0700

    Initial Commit

---
+++ TrackingPeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackingPeople
+//
+//  Created by Jose Preatorian on 03-10-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
